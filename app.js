c1e1e5406d418e40da5fc7b243bda5d839c5d848
dinosauce-285 <thanhbualqt@gmail.com>
2022-02-22 09:32:02 +0700

    fix xiu

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function bmi() {
     body.style.backgroundImage = "url('thin.webp')";
     body.style.backgroundPosition = "top";
 
-    cmt.innerHTML = `<h1 id="comment">thằng ${user.value} ốm vc</h1>
+    cmt.innerHTML = `<h1 >thằng ${user.value} ốm như con chó ghẻ </h1>
   `;
   }
   if (
@@ -18,13 +18,13 @@ function bmi() {
     a.value / (b.value * b.value) <= 24.9
   ) {
     body.style.backgroundImage = "url('nice.jpg')";
-    cmt.innerHTML = `<h1 id="comment">thằng ${user.value} cũng cân đối đó ku</h1>`;
+    cmt.innerHTML = `<h1 >thằng ${user.value} cũng cân đối đó ku</h1>`;
   }
   if (a.value / (b.value * b.value) >= 25) {
     body.style.backgroundImage = "url('fat.jpg')";
     body.style.backgroundPosition = "center";
     body.style.backgroundSize = "cover";
-    cmt.innerHTML = `<h1 id="comment">thằng ${user.value} là 1 con heo siêu đù </h1>`;
+    cmt.innerHTML = `<h1 >thằng ${user.value} là 1 con heo siêu đù </h1>`;
   }
 }
 button.onclick = bmi;
